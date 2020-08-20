79b24260496733842fb70f75a6b0b2da62264156
pp <1640675441@qq.com>
2020-08-21 00:16:38 +0800

    variable，temp变量

---
+++ Study/EMACjavascript/use_variable/savevariableforalert.js
@@ -0,0 +1,33 @@
+// 把输入弹框内容复制给变量input_name，有是少了分号;
+var input_name = prompt("请输入你的名字");
+// 弹框打印
+alert(input_name);
+// 控制台打印，放入内存
+console.log(input_name);
+// 变量声明
+var a;
+// 初始化多个变量值
+var b=1,
+    c=2,
+    d=3;
+// 变量不声明赋值，可以正常打印，但是不规范
+g = "aaa";
+console.log(g);
+// 变量声明后不赋值，会提示undefine
+var e;
+console.log(e);
+// 0开头是八进制
+var g = 012
+console.log(g) // 13
+
+// 变量不声明，会直接语法报错
+console.log(f);
+
+
+1280
+6400
+32000
+160000
+800000
+4000000
+10000000
